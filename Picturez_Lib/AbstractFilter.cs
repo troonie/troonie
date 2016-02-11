788bce02f84886ccc46e23e88164c09061ab2c58
leomartin1982 <picturez-project@t-online.de>
2016-02-11 15:30:33 +0100

    - Steganography algoritm added (still buggy).

---
+++ Picturez_Lib/AbstractFilter.cs
@@ -0,0 +1,46 @@
+﻿namespace Picturez_Lib
+{    
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
+    public abstract class AbstractFilter
+    {
+        /// <summary>
+        /// Applies the filter on the passed <paramref name="source"/> bitmap.
+        /// </summary>
+        /// <param name="source">The source image to process.</param>
+        /// <returns>The filter result as a new bitmap.</returns>
+        public Bitmap Apply(Bitmap source)
+        {
+            Bitmap destination = new Bitmap(
+                source.Width, source.Height, PixelFormat.Format32bppArgb);
+            Rectangle rect = new Rectangle(0, 0, source.Width, source.Height);
+            BitmapData srcData = source.LockBits(
+                rect, ImageLockMode.ReadWrite, source.PixelFormat);
+            BitmapData dstData = destination.LockBits(
+                rect, ImageLockMode.ReadWrite, destination.PixelFormat);
+            Process(srcData, dstData);
+            destination.UnlockBits(dstData);
+            source.UnlockBits(srcData);
+
+            if (destination.PixelFormat == PixelFormat.Format8bppIndexed)
+            {
+                // -- ColorPalette.SetColorPaletteToGray(destination); --
+                // get palette
+                var cp = destination.Palette;
+                // init palette
+                for (int i = 0; i < 256; i++)
+                {
+                    cp.Entries[i] = Color.FromArgb(i, i, i);
+                }
+                // set palette back
+                destination.Palette = cp;
+                // END -- ColorPalette.SetColorPaletteToGray(destination); --
+            }
+
+            return destination;
+        }
+
+        protected abstract void Process(BitmapData srcData, BitmapData dstData);
+    }
+}
---
+++ Picturez_Lib/AsciiInvertCharEncryption.cs
@@ -0,0 +1,95 @@
+﻿namespace Picturez_Lib
+{
+    using System;
+
+    public static class AsciiInvertCharEncryption
+    {
+        /// <summary>
+        /// Own Encoding.ASCII.GetBytes() method, because original one does not 
+        /// work correct for special characters (ä, ü, ...).
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static byte[] GetBytesFromString(string text)
+        {
+            Byte[] bytes = new byte[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                bytes[i] = (byte)text[i];
+            }
+
+            return bytes;
+        }
+
+        public static string Process(string text, int mod)
+        {
+            Byte[] bytes = GetBytesFromString(text);
+            string result = "";
+            
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (bytes[i] != 59 && mod == 0) // 59 == ASCII for ';'
+                {
+                    bytes[i] = (byte) (bytes[i] - 2);
+                }
+                else if (bytes[i] != 57 && mod == 1) // 57+2=59 == ASCII for ';'
+                {
+                    bytes[i] = (byte)(bytes[i] + 2);
+                }
+                result += (char)bytes[i];
+            }                        
+
+            return result;            
+        }
+    }
+
+    public static class Rc4
+    {
+        public static string Process(string text, Byte[] key)
+        {
+            Byte[] bytes = AsciiInvertCharEncryption.GetBytesFromString(text);
+
+            Byte[] s = new Byte[256];
+            Byte[] k = new Byte[256];
+            Byte temp;
+            int i;
+
+            for (i = 0; i < 256; i++)
+            {
+                s[i] = (Byte)i;
+                k[i] = key[i % key.GetLength(0)];
+            }
+
+            int j = 0;
+            for (i = 0; i < 256; i++)
+            {
+                j = (j + s[i] + k[i]) % 256;
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+
+            i = j = 0;
+            for (int x = 0; x < bytes.GetLength(0); x++)
+            {
+                i = (i + 1) % 256;
+                j = (j + s[i]) % 256;
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+                int t = (s[i] + s[j]) % 256;
+                bytes[x] ^= s[t];
+            }
+
+
+            string result = "";
+            foreach (byte b in bytes)
+            {
+                result += (char)b;
+            }
+
+            return result;
+        }
+    }
+}
