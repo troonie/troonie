d2782ffd0143a02708c4af770e934d4ae593c212
picturez-project <picturez-project@t-online.de>
2016-06-24 19:35:48 +0200

    Predefined starting point removed.

--- Picturez/Program.cs
+++ Picturez/Program.cs
@@ -60,7 +60,7 @@ namespace Picturez
 
 			string filename = null;
 			// START VALUE
-			 args = new string[] { "-e"};
+//			 args = new string[] { "-e"};
 
 			if (args.Length == 0) {
 				StarterWidget start = new StarterWidget (args);
