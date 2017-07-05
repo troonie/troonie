751c31404e90bc01b71d83917aab064ac0fb1a77
troonie <info@troonie.com>
2017-07-05 11:25:21 +0200

    [WinInstaller, TroonieInstaller] - Bugfix: When win installer was used with no admin user account, the installer extracted Troonie at admin desktop. Now win insatller extracts correctly at user desktop. - Project 'TroonieInstaller' added.

---
+++ TroonieInstaller/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GtkInstaller
+{
+	class MainClass
+	{
+		public static int Main(string[] args)
+		{
+			if (args.Length < 2) {
+				return 3;
+			}
+			int errorcode = 0;
+			string gtkInstaller = args[1];
+			string vcppMsiInstaller = args[0];
+			//string vcppCabInstaller = "cab1.cab";
+
+			// install gtk-sharp-2
+			Console.WriteLine("Installing GTK-Sharp-2.");
+			ProcessStartInfo info1 = new ProcessStartInfo();
+			info1.FileName = gtkInstaller;
+			info1.Arguments = "/Quiet /Passive /qn";
+			Process p1 = Process.Start(info1);
+			// wait max 2 minutes
+			if (p1 == null || !p1.WaitForExit(2 * 60 * 1000))
+			{
+				errorcode = 1;
+			}
+
+			// install Visual-C++-Redist-2013_x86
+			Console.WriteLine("Installing Visual-C++-Redist-2013.");
+			ProcessStartInfo info2 = new ProcessStartInfo();
+			info2.FileName = vcppMsiInstaller;
+			info2.Arguments = "/Quiet /Passive /qn";
+			Process p2 = Process.Start(info2);
+			// wait max 2 minutes
+			if (p2 == null || !p2.WaitForExit(2 * 60 * 1000))
+			{
+				errorcode = 2;
+			}
+
+			return errorcode;
+		}
+	}
+}
--- WinInstaller/InstallForm.cs
+++ WinInstaller/InstallForm.cs
@@ -7,141 +7,156 @@ using System.Windows.Forms;
 
 namespace WinInstaller
 {
-    public partial class InstallForm : Form
-    {
-		string[] t; 
-		int count;
+	public partial class InstallForm : Form
+	{
+		string[] t;
+		int errorcode;
+		string path;
 
 		public InstallForm()
-        {
-            InitializeComponent();
+		{
+			InitializeComponent();
 			Assembly thisExe = Assembly.GetExecutingAssembly();
 			Stream str = thisExe.GetManifestResourceStream("icon.ico");
-			this.Icon = new System.Drawing.Icon (str);
+			this.Icon = new System.Drawing.Icon(str);
 			t = new string[13];
-			bool l = IsGermanLanguage ();
+			bool l = IsGermanLanguage();
 
 			t[0] = l ? "Abbrechen" : "Cancel";
-			t[1] = l ? "Ok" : "Ok";
-			t[2] = l ? "INFO: GTK-Sharp oder Visual-C++-Redist-2013 wurden nicht korrekt installiert." : 
-					   "INFO: GTK-Sharp or Visual-C++-Redist-2013 were not installed correctly.";
-			t[3] = l ? "Troonie benötigt diese Komponenten. Bitte installiere Troonie noch einmal." : 
-						"Troonie needs these packages. Please re-install Troonie.";
-			t[4] = l ? "GTK-Sharp und Visual-C++-Redist-2013 wurden erfolgreich installiert." : 
+			t[1] = "Ok";
+			t[2] = l ? "FEHLER: GTK-Sharp oder Visual-C++-Redist-2013 wurden nicht " :
+					   "ERROR: GTK-Sharp or Visual-C++-Redist-2013 were not ";
+			t[3] = l ? "korrekt installiert. Troonie benötigt diese Komponenten." :
+						"installed correctly. Troonie needs these packages.";
+			t[4] = l ? "GTK-Sharp und Visual-C++-Redist-2013 wurden erfolgreich installiert." :
 					   "GTK-Sharp and Visual-C++-Redist-2013 were installed successfully.";
 			t[5] = l ? "Troonie wurde auf dem Desktop entpackt." : "Troonie was extracted at your Desktop.";
-			t[6] = l ? "Installiere... Bitte warten. " : "Installing... Please wait."; 
-			t[7] = l ? "Zuvor muss Windows neu gestartet werden, bevor ": 
-					   "Windows needs to be restarted before Troonie ";
-			t[8] = l ? "Troonie genutzt werden kann. Jetzt neu starten?" : "can be used. Restart now?";
+			t[6] = l ? "Installiere... Bitte warten. " : "Installing... Please wait.";
+			t[7] = l ? "Troonie starten? " : "Starting Troonie now? ";
+			t[8] = l ? "Bitte installiere Troonie noch einmal." : "Please re-install Troonie.";
 			t[9] = l ? "Der Installer entpackt Troonie auf den Desktop und " : "This installer extracts Troonie at your Desktop and ";
 			t[10] = l ? "installiert GTK-Sharp-2 sowie Visual-C++-Redist-2013." : "installs GTK-Sharp-2 as well as Visual-C++-Redist-2013.";
-			t[11] = l ? "Ja, neu starten" : "Yes, restart";
+			t[11] = l ? "Ja, Troonie starten" : "Yes, start Troonie";
 			t[12] = l ? "Nein, nur Installer beenden" : "No, just close installer";
 
 			btnOk.Text = t[1];
 			btnCancel.Text = t[0];
-			lbInfo.Text = t[9]  + Environment.NewLine + t[10] + Environment.NewLine;
-        }
+			lbInfo.Text = t[9] + Environment.NewLine + t[10] + Environment.NewLine;
+		}
 
-        private void btnOk_Click(object sender, EventArgs e)
-        {
-			if (count == 0) {
-				InstallGtksharpAndExtractTroonie ();
-			} else if (count == 1) {
-				Process.Start ("Shutdown", "-r -t 0");
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			if (errorcode == 0)
+			{
+				InstallGtksharpAndExtractTroonie();
 			}
-        }
+			else if (errorcode == 1)
+			{
+				//Process.Start ("Shutdown", "-r -t 0");
 
-        private void buttonCancel_Click(object sender, EventArgs e)
-        {
-			Close ();
-			Application.Exit ();
-        }
+				// start troonie
+				try
+				{
+					Close();
+					Application.Exit();
+
+					Process.Start(path + "Troonie.exe");
+				}
+				catch (Exception)
+				{
+				}
+			}
+		}
+
+		private void buttonCancel_Click(object sender, EventArgs e)
+		{
+			Close();
+			Application.Exit();
+		}
 
 		private void InstallGtksharpAndExtractTroonie()
 		{
 			btnOk.Visible = false;
 			btnCancel.Visible = false;
 			progressBar1.Visible = true;
-			lbInfo.Text = t [6];
-			this.Refresh ();
+			lbInfo.Text = t[6];
+			this.Refresh();
 
-			string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+			path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
 			path += Path.DirectorySeparatorChar + "Troonie" + Path.DirectorySeparatorChar;
-			Directory.CreateDirectory (path);
-			string gtkInstaller = path + "gtk-sharp-2.12.msi";
+			Directory.CreateDirectory(path);
+			string gtk_sharp_Installer = path + "gtk-sharp-2.12.msi";
 			string vcppMsiInstaller = path + "vc_runtimeMinimum_x86.msi";
 			string vcppCabInstaller = path + "cab1.cab";
+			string gtkInstaller = path + "TroonieInstaller.exe";
 
 			Assembly thisExe = Assembly.GetExecutingAssembly();
-			string [] resources = thisExe.GetManifestResourceNames();
+			string[] resources = thisExe.GetManifestResourceNames();
 
-			foreach (var item in resources) {
-				using (Stream str = thisExe.GetManifestResourceStream(item), 
+			foreach (var item in resources)
+			{
+				using (Stream str = thisExe.GetManifestResourceStream(item),
 					destStream = new FileStream(path + item, FileMode.Create, FileAccess.Write))
 				{
-					str.CopyTo (destStream);
+					str.CopyTo(destStream);
 				}
 			}
 
-			// install gtk-sharp-2
-			ProcessStartInfo info1 = new ProcessStartInfo();
-			info1.FileName = gtkInstaller;
-			info1.Arguments = "/Quiet /Passive /qn";
-			Process p1 = Process.Start(info1);
-			// wait max 3 minutes
-			if (p1 != null && p1.WaitForExit (3 * 60 * 1000)) {
-				count = 1;
-			}
+			Refresh();
 
-			this.Refresh ();
-				
 			// install Visual-C++-Redist-2013_x86
-			ProcessStartInfo info2 = new ProcessStartInfo();
-			info2.FileName = vcppMsiInstaller;
-			info2.Arguments = "/Quiet /Passive /qn";
-			Process p2 = Process.Start(info2);
+			ProcessStartInfo info = new ProcessStartInfo();
+			info.FileName = gtkInstaller;
+			info.Arguments = "\"" + vcppMsiInstaller + "\" \"" + gtk_sharp_Installer + "\"";
+			Process p = Process.Start(info);
 			// wait max 3 minutes
-			if (count == 1 && p2 != null && p2.WaitForExit (3 * 60 * 1000)) {
-				lbInfo.Text = t [4] + Environment.NewLine;
-			} else {
-				lbInfo.Text = t [2] + Environment.NewLine + t [3] + Environment.NewLine;
-				count = 0;
-			}				
-				
-			Thread.Sleep (50);
-			File.Delete (gtkInstaller);
-			File.Delete (vcppMsiInstaller);
-			File.Delete (vcppCabInstaller);
+			if (p != null && p.WaitForExit(3 * 60 * 1000) && p.ExitCode == 0 )
+			{
+				lbInfo.Text = t[4] + Environment.NewLine;
+				errorcode = 1;
+			}
+			else {
+				lbInfo.Text = t[2] + Environment.NewLine + t[3] + Environment.NewLine + Environment.NewLine + t[8];
+				errorcode = 0;
+			}
+			//Console.WriteLine("ExitCode: " + p2.ExitCode);
+
+			Refresh();
+
+			Thread.Sleep(50);
+			File.Delete(gtk_sharp_Installer);
+			File.Delete(vcppMsiInstaller);
+			File.Delete(vcppCabInstaller);
+			File.Delete(gtkInstaller);
 
 			btnCancel.Visible = true;
 			progressBar1.Visible = false;
 
-			if (count == 0) {
+			if (errorcode == 0)
+			{
 				return;
-			} else {
+			}
+			else {
 				btnOk.Visible = true;
 				btnOk.Text = t[11];
 				btnCancel.Text = t[12];
 			}
 
-			string tt = t [5] + Environment.NewLine + 
-						" (" + path + ") " + Environment.NewLine + Environment.NewLine + 
-						t [7] + Environment.NewLine +
-						t [8] + Environment.NewLine;
+			string tt = t[5] + Environment.NewLine +
+						" (" + path + ") " + Environment.NewLine + Environment.NewLine + t[7] + Environment.NewLine;
 			lbInfo.Text = lbInfo.Text + tt;
-		}			
+		}
 
 		private static bool IsGermanLanguage()
 		{
-			switch (System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName) {
-			case "de":
-				return true;
-			case "en":
-			default:
-				return false;
+			switch (System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName)
+			{
+				case "de":
+					return true;
+				case "en":
+				default:
+					return false;
 			}
-		}			
-    }
+		}
+	}
 }
